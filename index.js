0485dca589710a74bd5f36ef3c9319a36d184b53
VaibhavWadhwa <37235692+VaibhavWadhwa@users.noreply.github.com>
2023-06-12 12:40:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const config = require('./config/config');
 const userRouter = require('./routes/user');
@@ -20,23 +21,24 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/user", userRouter);
-app.use("/story", storyRouter);
-app.use("/payment",checkoutRouter)
-app.get("/", auth, (req, res) => {
+app.use("/api/user", userRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/payment", checkoutRouter)
+app.get("/api/getCookie", auth, (req, res) => {
     res.status(200).json({
         err: false,
         message: "success",
         jwt: req.cookies.jwt || "",
         username: req?.user?.name || "",
-        email : req?.user?.email || "",
-        courses : req?.user?.courses || []
+        email: req?.user?.email || "",
+        courses: req?.user?.courses || []
     })
 })
-app.use(express.static(‘client/build’));
-app.get(‘*’, (req, res) => {
-res.sendFile(path.join(__dirname + ‘/client/build/index.html’));
+app.use(express.static("client/build"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+
 app.listen(config.PORT, () => {
     MongoClient.connect("mongodb+srv://Aishwarya:Aishwarya@cluster0.n2jwfug.mongodb.net/?retryWrites=true&w=majority").then(res => {
         console.log("connected to Mongo DB")
